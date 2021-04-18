b67383427e974cfb9134f7dfcc4be658c2052e64
Terrence721 <terrence_daniels_35@yahoo.com>
2021-04-18 14:13:52 -0400

    WIP

--- 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
+++ 05-interacting-within-a-component/begin/vue-heroes/src/components/heroes.vue
@@ -27,7 +27,7 @@
       <div class="column is-4" v-if="selectedHero">
         <div class="card">
           <header class="card-header">
-            <p class="card-header-title">{{ selectedHero.firstName }}</p>
+            <p class="card-header-title">{{ fullName }}</p>
           </header>
           <div class="card-content">
             <div class="content">
@@ -61,6 +61,32 @@
                   v-model="selectedHero.description"
                 />
               </div>
+              <div class="field">
+                <label class="label" for="originDate">origin date</label>
+                <input
+                  type="date"
+                  class="input"
+                  id="originDate"
+                  v-model="selectedHero.originDate"
+                />
+                <p class="comment">
+                  My origin story began on
+                  {{ selectedHero.originDate | shortDate }}
+                </p>
+              </div>
+              <div class="field">
+                <label class="label" for="capeCounter">cape counter</label>
+                <input
+                  class="input"
+                  id="capeCounter"
+                  type="number"
+                  v-model="selectedHero.capeCounter"
+                />
+              </div>
+              <div class="field">
+                <label class="label" for="capeMessage">cape message</label>
+                <label class="input" name="capeMessage">{{ capeMessage }}</label>
+              </div>
             </div>
           </div>
           <footer class="card-footer">
@@ -83,35 +109,74 @@
 </template>
 
 <script>
+import { format } from 'date-fns';
+const inputDateFormat = 'YYYY-MM-DD';
+const displayDateFormat = 'MMM DD, YYYY';
 const ourHeroes = [
   {
     id: 10,
-    firstName: 'Ella',
-    lastName: 'Papa',
+    firstName: 'Terrence',
+    lastName: 'Daniels',
+    capCounter: 1,
+    originDate: format(new Date(1996, 5, 1), inputDateFormat),
     description: 'fashionista',
   },
   {
     id: 20,
-    firstName: 'Madelyn',
-    lastName: 'Papa',
+    firstName: 'Lawson',
+    lastName: 'Brandon',
+    capCounter: 3,
+    originDate: format(new Date(1998, 7, 1), inputDateFormat),
     description: 'the cat whisperer',
   },
   {
     id: 30,
-    firstName: 'Haley',
-    lastName: 'Papa',
+    firstName: 'Tammy',
+    lastName: 'Daniels',
+    capCounter: 2,
+    originDate: format(new Date(1999, 8, 1), inputDateFormat),
     description: 'pen wielder',
   },
   {
     id: 40,
-    firstName: 'Landon',
-    lastName: 'Papa',
+    firstName: 'Rubie',
+    lastName: 'Daniels',
+    capCounter: 0,
+    originDate: format(new Date(2000, 9, 1), inputDateFormat),
     description: 'arc trooper',
   },
 ];
 export default {
   name: 'Heroes',
+  data() {
+    return {
+      heroes: [],
+      selectedHero: undefined,
+      message: '',
+      capeMessage: '',
+    };
+  },
+  computed: {
+    fullName() {
+      return `${this.selectedHero.firstName} ${this.selectedHero.lastName}`;
+    },
+  },
+  created() {
+    this.loadHeroes();
+  },
+
   methods: {
+    async getHeroes() {
+      return new Promise(resolve => {
+        setTimeout(() => resolve(ourHeroes), 1500);
+      });
+    },
+    async loadHeroes() {
+      this.heroes = [];
+      this.message = 'getting the heroes. please be patient';
+      this.heroes = await this.getHeroes();
+      this.message = '';
+    },
     handleTheCapes(newValue) {
       const value = parseInt(newValue, 10);
       switch (value) {
@@ -139,6 +204,20 @@ export default {
     },
     selectHero(hero) {
       this.selectedHero = hero;
+    },
+  },
+  watch: {
+    'selectedHero.capCounter': {
+      immediate: true,
+      handler(newValue, oldValue) {
+        console.log(`Watcher evalauted. old=${oldValue}, new=${newValue}`);
+        this.handleTheCapes(newValue);
+      },
+    },
+  },
+  filters: {
+    shortDate: function(value) {
+      return format(value, displayDateFormat);
     },
   },
 };
--- 05-interacting-within-a-component/begin/vue-heroes/vue.config.js
+++ 05-interacting-within-a-component/begin/vue-heroes/vue.config.js
@@ -2,4 +2,8 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map',
   },
+  chainWebpack: config => {
+    config.module.rules.delete('eslint');
+}
 };
+
